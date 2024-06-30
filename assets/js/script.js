4af7b47906eb2cc2aa6bf47a9330f5395326bde3
sojibahmedev <sojibahmed.tic@gmail.com>
2024-06-30 16:59:14 +0600

    responsive done with panel

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,15 +36,149 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 // ideals section end
 // panel script start
-function togglePanel() {
-  var panel = document.getElementById("panel");
-  panel.classList.toggle("open");
+function toggleMenu() {
+  const panel = document.getElementById('sidePanel');
+  const body = document.getElementById('idparentcontainer');
+
+  panel.classList.toggle('open');
+  body.classList.toggle('dimmed');
+
+  if (panel.classList.contains('open')) {
+      document.addEventListener('click', closeMenuOnClickOutside);
+  } else {
+      document.removeEventListener('click', closeMenuOnClickOutside);
+  }
 }
 
-document.addEventListener("click", function(event) {
-  var panel = document.getElementById("panel");
-  if (!panel.contains(event.target) && panel.classList.contains("open")) {
-      panel.classList.remove("open");
+function closeMenuOnClickOutside(event) {
+  const panel = document.getElementById('sidePanel');
+  const menuIcon = document.querySelector('.nav-right-ham-burger-menu');
+  const body = document.getElementById('idparentcontainer');
+
+  if (!panel.contains(event.target) && !menuIcon.contains(event.target)) {
+      panel.classList.remove('open');
+      body.classList.remove('dimmed');
+      document.removeEventListener('click', closeMenuOnClickOutside);
   }
-});
+}
+
+
+
 // panel script end
+
+
+// panel sub-item script start
+function toggleSubItem() {
+  const subPanel = document.getElementById('subPanel');
+  const icon = document.querySelector('.whyadopt-sidepanel > i');
+
+  if (subPanel.style.display === 'none' || subPanel.style.display === '') {
+      subPanel.style.display = 'block';
+      icon.classList.add('icon-rotate');
+  } else {
+      subPanel.style.display = 'none';
+      icon.classList.remove('icon-rotate');
+  }
+}
+
+// panel sub-item script end
+
+// subpanel sub-item-script start
+function toggleSubItemSubChild() {
+  const subPanelChild = document.getElementById('subPanelChild');
+  const icon = document.querySelector('.subpanel-subitem-contents > i');
+
+  if (subPanelChild.style.display === 'none' || subPanelChild.style.display === '') {
+    subPanelChild.style.display = 'block';
+      icon.classList.add('icon-rotate');
+  } else {
+    subPanelChild.style.display = 'none';
+      icon.classList.remove('icon-rotate');
+  }
+}
+function toggleSubItemSubChildPlatform() {
+  const subPanelChild = document.getElementById('subPanelChildPlatform');
+  const icon = document.querySelector('.subitem-arrow-rotate-use');
+
+  if (subPanelChild.style.display === 'none' || subPanelChild.style.display === '') {
+    subPanelChild.style.display = 'block';
+      icon.classList.add('icon-rotate');
+  } else {
+    subPanelChild.style.display = 'none';
+      icon.classList.remove('icon-rotate');
+  }
+}
+
+function toggleSubItemSubChildAdopt() {
+  const subPanelChild = document.getElementById('subPanelChildPlatformAdopt');
+  const icon = document.querySelector('.adopt-sub-child-rotate');
+
+  if (subPanelChild.style.display === 'none' || subPanelChild.style.display === '') {
+    subPanelChild.style.display = 'block';
+      icon.classList.add('icon-rotate');
+  } else {
+    subPanelChild.style.display = 'none';
+      icon.classList.remove('icon-rotate');
+  }
+}
+// subpanel sub-item-script end
+
+
+// sidepanel solution start
+function toggleSolution() {
+  const subPanelChild = document.getElementById('subPanelSolution');
+  const icon = document.querySelector('.sidepanel-solution-arrow');
+
+  if (subPanelChild.style.display === 'none' || subPanelChild.style.display === '') {
+    subPanelChild.style.display = 'block';
+      icon.classList.add('icon-rotate');
+  } else {
+    subPanelChild.style.display = 'none';
+      icon.classList.remove('icon-rotate');
+  }
+}
+
+// personas section start
+function toggleSubItemSubChildPersonas() {
+  const subPanelChild = document.getElementById('subPanelChildPersonasItems');
+  const icon = document.querySelector('.subitem-arrow-rotate-use-personas');
+
+  if (subPanelChild.style.display === 'none' || subPanelChild.style.display === '') {
+    subPanelChild.style.display = 'block';
+      icon.classList.add('icon-rotate');
+  } else {
+    subPanelChild.style.display = 'none';
+      icon.classList.remove('icon-rotate');
+  }
+}
+// personas section end
+// use cases section start
+function toggleSubItemSubChildUseCases() {
+  const subPanelChild = document.getElementById('subPanelChildUsecasesItems');
+  const icon = document.querySelector('.subitem-arrow-rotate-use-usecases');
+
+  if (subPanelChild.style.display === 'none' || subPanelChild.style.display === '') {
+    subPanelChild.style.display = 'block';
+      icon.classList.add('icon-rotate');
+  } else {
+    subPanelChild.style.display = 'none';
+      icon.classList.remove('icon-rotate');
+  }
+}
+// use cases section start
+
+// sidepanel solution end
+// panel-resources section start
+function toggleSubItemSubChildResources() {
+  const subPanelChild = document.getElementById('toggleSubItemSubChildResourcesSubItem');
+  const icon = document.querySelector('.subitem-arrow-rotate-use-resources');
+
+  if (subPanelChild.style.display === 'none' || subPanelChild.style.display === '') {
+    subPanelChild.style.display = 'block';
+      icon.classList.add('icon-rotate');
+  } else {
+    subPanelChild.style.display = 'none';
+      icon.classList.remove('icon-rotate');
+  }
+}
+// panel-resources section end
